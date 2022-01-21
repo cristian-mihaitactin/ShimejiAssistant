aabe51931fc23ba6be9e783f3e5a06cb3f5e5dd5
cristian-mihaitactin <cristian_mihaita.ctin@yahoo.com>
2022-01-21 16:32:03 +0200

    Add Plugin DB entities

---
+++ Barn/Barn.Entities/Plugins/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barn.Entities.Plugins
+{
+    public class Plugin: EntityWithRowVersion
+    {
+        public Guid Id { get; set; }
+        public IList<PluginNotification> PluginNotifications { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Version { get; set; }
+    }
+}
---
+++ Barn/Barn.Entities/Plugins/PluginNotification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Barn.Entities.Bucky;
+
+namespace Barn.Entities.Plugins
+{
+    public class PluginNotification : EntityWithRowVersion
+    {
+        public Guid Id { get; set; }
+        public ActionType ActionType { get; set; }
+        public string Message { get; set; }
+        public Guid PluginId { get; set; }
+        public Plugin Plugin { get; set; }
+    }
+}
