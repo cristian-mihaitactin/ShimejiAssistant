b77b6599defa9f50aa8f81358d30441f10873454
cristian-mihaitactin <cristian_mihaita.ctin@yahoo.com>
2022-01-24 16:03:36 +0200

    Add/adjust plugin components and intergration

---
+++ Bucky-angular/Mane/src/app/mainview/components/plugin-sample/plugin-sample.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plugin-sample',
+  templateUrl: './plugin-sample.component.html',
+  styleUrls: ['./plugin-sample.component.css']
+})
+export class PluginSampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Bucky-angular/Mane/src/app/mainview/mainview.module.ts
+++ Bucky-angular/Mane/src/app/mainview/mainview.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IntegratedServicesComponent } from './components/plugins/plugins.component';
+import { PluginSampleComponent } from './components/plugin-sample/plugin-sample.component';
 import { AssistantProfileComponent } from './components/assistant-profile/assistant-profile.component';
 import { BuckyProfileSampleComponent } from './components/bucky-profile-sample/bucky-profile-sample.component';
 import { PluginDetailsComponent } from './components/plugin-details/plugin-details.component';
@@ -9,7 +9,7 @@ import { PluginDetailsComponent } from './components/plugin-details/plugin-detai
   imports: [
     CommonModule
   ],
-  exports: [IntegratedServicesComponent, AssistantProfileComponent, BuckyProfileSampleComponent],
-  declarations: [IntegratedServicesComponent, AssistantProfileComponent, BuckyProfileSampleComponent, PluginDetailsComponent]
+  exports: [PluginSampleComponent, PluginDetailsComponent, AssistantProfileComponent, BuckyProfileSampleComponent],
+  declarations: [PluginSampleComponent, PluginDetailsComponent, AssistantProfileComponent, BuckyProfileSampleComponent]
 })
 export class MainviewModule { }
